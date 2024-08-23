b5dcf5e773afd8b780adc608289b84d22440d57d
Mozhgan <mojganshah@gmail.com>
2024-08-23 18:38:50 +0330

    dom elements added

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 // const months = ["January", "February", "March", "April", "May", "June", "July",
 //     "August", "September", "October", "November", "December"];
+
+const $ = document;
+const popUpEl = $.querySelector(".popup-box");
+const addBox = $.querySelector(".add-box");
+const titleEl = $.querySelector("header p");
+const closeEl = $.querySelector("header i");
+const inputEl = $.querySelector("input");
+const descriptionEl = $.querySelector("textarea");
+const btnElm = $.querySelector("button");
+
+let notes = [];
+
+function getLocalStorageData() {
+  let LocalStorageData = localStorage.getItem("notes");
+  console.log(LocalStorageData);
+}
+getLocalStorageData();
